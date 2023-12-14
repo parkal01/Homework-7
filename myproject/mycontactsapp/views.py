2102c8b2866ade58016d578dec18cdcf63862d0d
parkal01 <sampathreddyparkala@gmail.com>
2023-12-14 07:30:42 +0300

    first commit

---
+++ myproject/mycontactsapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+# import Contact from Models
+from django.http import HttpResponse
+from .models import Contact
+
+def index(request):
+    #return HttpResponse('Hello World')
+    # Display contacts
+    mylist = ""
+    for i in Contact.objects.all():
+        mylist += i.first_name + ', ' #update this code to list first name and last name as below
+        # Henry Fox
+        # James Brown
+        # Kate Charnes
+        # Make sure the list is alphabetically sorted.
+    return HttpResponse("All Contacts:<br>" + mylist) 
+    # update return: Center the list, 
+    # and make two other css modifications as you wish.
